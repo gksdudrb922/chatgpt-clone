31e8db7f088858e80563a3b4e4102ca76c032ab4
gksdudrb922 <gksdudrb922@gmail.com>
2025-10-23 02:09:37 +0900

    test: test streamlit

---
+++ main.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import time
+
+st.write("Hello world!")
+
+st.button("Click me please!")
+
+st.text_input("Write your API KEY", max_chars=20)
+
+st.feedback("faces")
+
+with st.sidebar:
+    st.badge("Badge 1")
+
+tab1, tab2, tab3 = st.tabs(["Agent", "Chat", "Output"])
+
+with tab1:
+    st.header("Agent")
+with tab2:
+    st.header("Agent2")
+with tab3:
+    st.header("Agent3")
+
+with st.chat_message("ai"):
+    st.text("Hello!")
+    with st.status("Agent is using tool") as status:
+        time.sleep(1)
+        status.update(label="Agent is searching the web....")
+        time.sleep(2)
+        status.update(label="Agent is reading the page....")
+        time.sleep(3)
+        status.update(state="complete")
+with st.chat_message("human"):
+    st.text("Hi!")
+
+st.chat_input("Write a message for the assistant.", accept_file=True)
