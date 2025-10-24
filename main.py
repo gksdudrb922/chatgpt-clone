5fafc8e19be7d3d24d4dbae0e21ddb1b32e07a5e
gksdudrb922 <gksdudrb922@gmail.com>
2025-10-25 00:31:44 +0900

    test: test session memory

--- main.py
+++ main.py
@@ -1,36 +1,20 @@
 import streamlit as st
-import time
 
-st.write("Hello world!")
+if "is_admin" not in st.session_state:
+    st.session_state["is_admin"] = False
 
-st.button("Click me please!")
+def setup_application():
+    print("fetch an api....")
 
-st.text_input("Write your API KEY", max_chars=20)
+setup_application()
 
-st.feedback("faces")
+st.header("Hello!")
 
-with st.sidebar:
-    st.badge("Badge 1")
+name = st.text_input("What is your name?")
 
-tab1, tab2, tab3 = st.tabs(["Agent", "Chat", "Output"])
+if name:
+    st.write(f"Hello {name}")
+    
+    st.session_state["is_admin"] = True
 
-with tab1:
-    st.header("Agent")
-with tab2:
-    st.header("Agent2")
-with tab3:
-    st.header("Agent3")
-
-with st.chat_message("ai"):
-    st.text("Hello!")
-    with st.status("Agent is using tool") as status:
-        time.sleep(1)
-        status.update(label="Agent is searching the web....")
-        time.sleep(2)
-        status.update(label="Agent is reading the page....")
-        time.sleep(3)
-        status.update(state="complete")
-with st.chat_message("human"):
-    st.text("Hi!")
-
-st.chat_input("Write a message for the assistant.", accept_file=True)
+print(st.session_state["is_admin"])
